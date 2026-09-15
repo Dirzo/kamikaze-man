54cc9ae4784840ead1157aeb2ca40f08bb907e58
Dirzo <86633082+Dirzo@users.noreply.github.com>
2026-09-15 14:54:54 -0600

    Stamp browser-validated Heavy build

--- crazy-weapon-man-site/src/entry.js
+++ crazy-weapon-man-site/src/entry.js
@@ -1,6 +1,6 @@
 import app from './index.js';
 
-const BUILD = 'cwl-heavy-live-20260915n';
+const BUILD = 'cwl-heavy-live-20260915o';
 
 function withHeaders(headers = new Headers()) {
   const h = new Headers(headers);
