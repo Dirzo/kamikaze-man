fb2ebf232ba7789a94424ff8ffd227958fa9a55b
Dirzo <86633082+Dirzo@users.noreply.github.com>
2026-09-15 16:20:49 -0600

    Stamp cleaned Heavy combat presentation release

--- crazy-weapon-man-site/src/entry.js
+++ crazy-weapon-man-site/src/entry.js
@@ -1,6 +1,6 @@
 import app from './index.js';
 
-const BUILD = 'cwl-heavy-live-20260915q';
+const BUILD = 'cwl-heavy-live-20260915r';
 
 function withHeaders(headers = new Headers()) {
   const h = new Headers(headers);
